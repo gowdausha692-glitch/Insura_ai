56008e83900398318ce3f62e5d38261ff2daaf93
gowdausha692-glitch <gowdausha692@gmail.com>
2026-09-22 09:55:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ from sklearn.preprocessing import LabelEncoder
 st.set_page_config(
     page_title="InsuraAI",
     page_icon="🛡️",
-    layout="wide",
+    layout="justified",
     initial_sidebar_state="expanded",
 )
 
